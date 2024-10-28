c0876f2c71767f735500c4f6be8843b27b8ac677
NaudCode <naudcode@gmail.com>
2024-10-28 21:27:58 +0100

    add cover image h1 title

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
-                        <h1 class="mx-auto my-0 text-uppercase">Bretignolles-sur-Mer <br> France</h1>
+                        <h1 class="mx-auto my-0 text-uppercase">Bretignolles <br> sur-Mer</h1>
                         <h2 class="text-white-50 mx-auto mt-2 mb-5"><?php echo $firstH2 ?></h2>
                         <a class="btn btn-primary" href="#about"><?php echo $start ?></a>
                     </div>
@@ -64,15 +64,11 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8">
-                        <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
-                        <p class="text-white-50">
-                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
-                            The theme is open source, and you can use it for any purpose, personal or commercial.
-                        </p>
+                        <h2 class="text-white mb-4"><?php echo $house ?></h2>
+                        <p class="text-white-50"><?php echo $descHouse ?></p>
                     </div>
                 </div>
-                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
+                <img class="img-fluid" src="assets/img/veranda_cosy.jpeg" alt="..." />
             </div>
         </section>
         <!-- Projects-->
--- langage/fr.php
+++ langage/fr.php
@@ -1,5 +1,6 @@
 <?php
 
+//nav bar
  $about = "à Propos de";
  $photo = "photos";
  $location = "localisation";
@@ -7,8 +8,17 @@
  $activity = "activités";
  $contact = "contact";
 
+// Cover Page
  $start = "découvrir";
  $firstH2 = "Profitez d'une maison spacieuse au bord de la côte vendéenne.";
+
+//Presentation House
+ $house = "Profitez d'un logement complet avec piscine privative.";
+ $descHouse = "Jolie maison de 3 chambres avec jardin entièrement cloturé comprenant une piscine sécurisée d'accès par dôme. <br> 
+ Située à 300 mètres de l'océan (chemin de promenade qui emmène à 10 min à pied aux plages et commerces de la parée ou de la sauzaie). <br>
+ Commerce vival, restaurant à 5 min à pied.<br> 
+ Profitez d'un jardin, barbecue, terrasse ou véranda. Notre maison est une maison familiale, avec à disposition le nécessaire de cuisine (sel, poivre, huile ), 
+ cafetière à grain, dolce Gusto, sodastream, … coin livre, jeux enfants, …";
 
  //alt photos
 
