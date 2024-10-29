8f916a371022ba4c9c7b8957d3b18f71a3e9a51a
NaudCode <naudcode@gmail.com>
2024-10-29 20:53:44 +0100

    Navbar white to black + boder underline / activity

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#page-top">Locations Bachelier</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -73,26 +73,34 @@
         </section>
         <!-- Projects-->
         <section class="projects-section bg-light" id="projects">
+
+            <div class="text-center"><p><h2><?php echo $TitleActivity ?></h2></p></div>
             <div class="container px-4 px-lg-5">
+
                 <!-- Featured Project Row-->
-                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/" alt="" /></div>
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/babyfoot.png" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white"><?php echo $hobbiesTitle ?></h4>
+                                    <p class="mb-0 text-white-50"><?php echo $hobbies ?></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <!-- Project One Row-->
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/salon.avif" alt="..." /></div>
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
-                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                    <h4 class="text-white"><?php echo $tvTitle ?></h4>
+                                    <p class="mb-0 text-white-50"><?php echo $tv ?></p>
                                 </div>
                             </div>
                         </div>
@@ -100,13 +108,13 @@
                 </div>
                 <!-- Project Two Row-->
                 <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/piscine_securisee.jpg" alt="..." /></div>
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
-                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well!</p>
+                                    <h4 class="text-white"><?php echo $swimpoolTitle ?></h4>
+                                    <p class="mb-0 text-white-50"><?php echo $swimpool ?></p>
                                 </div>
                             </div>
                         </div>
--- langage/fr.php
+++ langage/fr.php
@@ -15,11 +15,31 @@
 //Presentation House
  $house = "Profitez d'un logement complet avec piscine privative.";
  $descHouse = "Jolie maison de 3 chambres avec jardin entièrement cloturé comprenant une piscine sécurisée d'accès par dôme. <br> 
- Située à 300 mètres de l'océan (chemin de promenade qui emmène à 10 min à pied aux plages et commerces de la parée ou de la sauzaie). <br>
- Commerce vival, restaurant à 5 min à pied.<br> 
- Profitez d'un jardin, barbecue, terrasse ou véranda. Notre maison est une maison familiale, avec à disposition le nécessaire de cuisine (sel, poivre, huile ), 
- cafetière à grain, dolce Gusto, sodastream, … coin livre, jeux enfants, …";
+    Située à 300 mètres de l'océan (chemin de promenade qui emmène à 10 min à pied aux plages et commerces de la parée ou de la sauzaie). <br>
+    Commerce vival, restaurant à 5 min à pied.<br> 
+    Profitez d'un jardin, barbecue, terrasse ou véranda. Notre maison est une maison familiale, avec à disposition le nécessaire de cuisine (sel, poivre, huile ), 
+    cafetière à grain, dolce Gusto, sodastream, … coin livre, jeux enfants, …";
+
+
+//Activities
+
+ $TitleActivity = "Nos mise à disposition";
+
+ $hobbiesTitle = "Activités";
+ $hobbies = "Profitez de différentes activitées mise à votre disposition, tel que que jeux de société ou de notre babyfoot de superbe qualité !
+    <br>Vous trouverez aussi différents jouets mis à disposition dans la chambre d'enfant.
+    <br>Ainsi qu'une petite bibliothèque pour lire paisiblement dans la véranda ou à la piscine.";
+
+ $tvTitle = "Multimédia";    
+ $tv = "Restez connecté, un accès wifi est mise à disposition. De plus vous pouvez profiter de téléviseurs disposé dans les chambres adultes, salon et véranda. 
+    Ces téléviseurs sont connectés via Android TV, afin de pouvoir caster des vidéos depuis son smartphone.";    
+  
+ $swimpoolTitle = "Piscine";
+ $swimpool = "Savourez de notre piscine de 0.6m à 2m de profondeur. Traité à l'eau salé et autonome dans la régulation du PH, pas de sensation de chlore sur la peau et cheveux. 
+    Celle-ci est couverte par un dome de 1.8m de hauteur, et verouillage par clé pour la sécurité des enfants." 
 
  //alt photos
+
+
 
 ?>
