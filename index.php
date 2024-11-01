3bf52116262e58294cfce6252a760026b6b24b98
NaudCode <naudcode@gmail.com>
2024-11-01 17:29:03 +0100

    improvement

--- index.php
+++ index.php
@@ -290,7 +290,7 @@
         </section>
 
         <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2023</div></footer>
+        <footer class="footer bg-black small text-center text-white-50"></footer>
         
         <!-- Cookies Promp -->
         <div id="cookie-bar">
